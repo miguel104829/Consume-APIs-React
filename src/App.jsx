ad8836e8c01e182186daea495cabfec3d4fec0bc
miguel104829 <miguel_samaca@hotmail.com>
2023-10-05 16:32:54 -0500

    Upload project files

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+/*import { useFetch } from './useFetch';
+import './App.css'
+
+function App() {
+  const { data, loading, error, handleCancelRequest } = useFetch('https://jsonplaceholder.typicode.com/users');  
+  
+  return (
+    <div className='App'>
+      <h1>Fetch like a PRO</h1>
+      <button onClick={handleCancelRequest}>Cancel Request</button>
+      <div className='card'>
+        <ul>
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading...</li>}
+          {data?.map((user) => (<li key={user.id}>{user.name}</li>))}
+        </ul>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App
+*/
+import { Suspense } from 'react';  
+import { fetchData } from './fetchData';
+import './App.css'
+
+const apiData = fetchData('https://jsonplaceholder.typicode.com/users');
+
+function App() {
+  const data = apiData.read();
+  return (
+    <div className='App'>
+      <h1>Fetch like a PRO</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+          <ul className='card'>
+            {data?.map((user) => (<li key={user.id}>{user.name}</li>))}
+          </ul>
+      </Suspense>
+    </div>
+  );
+}
+export default App;
